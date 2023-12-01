3c995ee60a5769d44769cf838ae9ba614e4f77d6
Mark-W-Miller <mark.miller@moondance.com>
2023-12-01 01:43:42 +0100

    First

---
+++ play/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+# Create a random numpy array
+arr = np.random.rand(3, 3)
+print(arr)
+# Create a pandas dataframe
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+print(df)
